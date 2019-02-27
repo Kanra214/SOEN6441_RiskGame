20af51ae92894a55b253e3c79e86d31c6a49cf0b
Binbin6 <binxue6@gmail.com>
2019-02-26 22:48:56 -0500

    New mapload function

--- src/Game/MapLoader.java
+++ src/Game/MapLoader.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import Models.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Map;
 
 /**
  * In Game package and this class is for loading the map
@@ -15,9 +16,16 @@ import java.io.FileReader;
 public class MapLoader {
     public String author;
     public String mapname;
+    public MapEdit map;
     ArrayList<Continent> worldmap = new ArrayList<>();
     ArrayList<Country> graph = new ArrayList<>();
+    ArrayList<ArrayList> result = new ArrayList<>();
+    public MapLoader() {
+        map = new MapEdit("a");
+    }
 
+
+   //Don't use this function
     public ArrayList<ArrayList> load(String filePath) throws IOException {
 
         BufferedReader reader = new BufferedReader(new FileReader("entry.txt"));
@@ -73,7 +81,7 @@ public class MapLoader {
 
                             input = reader.readLine();
                         }
-                        ArrayList<ArrayList> result = new ArrayList<>();
+//                        ArrayList<ArrayList> result = new ArrayList<>();
                         result.add(graph);
                         result.add(worldmap);
                         return result;
@@ -109,7 +117,49 @@ public class MapLoader {
         return null;
     }
 
+    // This function is used to load map start game(include map validation)
+    public ArrayList<ArrayList> loadMap(String mapName){
+        Map<String,ArrayList<Country>> countries ;
+        Map<String,ArrayList<String>> adjacencyList;
+        map.loadMapFile(mapName);
+        countries = map.countries;
+        adjacencyList = map.adjacencyList;
+        worldmap = map.continents;
+        for (ArrayList<Country> tempCountryList : countries.values()){
+            for (Country tempCountry : tempCountryList){
+                graph.add(tempCountry);
+            }
+        }
+        for (Continent tempContinent : worldmap) {
+            for (String continentname : countries.keySet()) {
+                if (tempContinent.getName().equals(continentname)) {
+                    for (Country tempCountry : countries.get(continentname)){
+                        tempContinent.getCountries().add(tempCountry);
+                    }
+                }
+            }
 
+        }
+        for (Country tempCountry : graph) {
+            for (String countryName : adjacencyList.keySet()) {
+                if (tempCountry.getName().equals(countryName)) {
+                    for (String neighbourName : adjacencyList.get(countryName)) {
+                        for (Country neighbourCountry : graph) {
+                            if (neighbourCountry.getName().equals(neighbourName)) {
+                                tempCountry.getNeighbours().add(neighbourCountry);
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        result.add(graph);
+        result.add(worldmap);
+        System.out.println(1);
+        return result;
+    }
 
 
 }
