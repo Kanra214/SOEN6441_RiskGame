6603e112d27ce4597ef5d3c392b0138793d1d177
JiaHui-Wang <wjhmars@126.com>
2019-03-12 22:46:22 -0400

    AttackCheck

--- src/Game/Controller.java
+++ src/Game/Controller.java
@@ -117,10 +117,7 @@ public class Controller {
                     if(!checkAttack(p.getCurrent_player())){
                         p.nextPhase();
                     }
-                    if(!checkAttack(p.getCurrent_player())){
-                        System.out.println("1");
-                        p.nextPhase();
-                    }
+                    
                     if (chosenFrom == null) {
                         chosenFrom = chosen;
                     } else {
